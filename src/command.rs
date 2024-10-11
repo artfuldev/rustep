a0493f5634bb57a660957959739df042ba78dbec
Sudarsan Balaji <hello@artful.dev>
2024-10-11 17:40:52 +0100

    Verify validity of versions

--- src/command.rs
+++ src/command.rs
@@ -1,7 +1,7 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{multispace1, u8};
-use nom::combinator::value;
+use nom::combinator::{value, verify};
 use nom::sequence::{preceded, tuple};
 use nom::IResult;
 
@@ -16,7 +16,7 @@ impl Command {
     fn parse_handshake(input: &str) -> IResult<&str, Command> {
         let (remaining, version) = preceded(
             tuple((tag("st3p"), multispace1, tag("version"), multispace1)),
-            u8,
+            verify(u8, |version: &u8| *version >= 1 && *version <= 2),
         )(input)?;
         Ok((remaining, Command::Handshake(version)))
     }
