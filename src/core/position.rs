3b1b132235f277a10ed2f98ff76917ee2d5adfdd
Sudarsan Balaji <hello@artful.dev>
2024-10-12 18:33:34 +0100

    Fix move representation

--- src/core/position.rs
+++ src/core/position.rs
@@ -2,6 +2,8 @@ use std::fmt::Display;
 
 use num::BigUint;
 
+use super::Move;
+
 fn column(mut index: u16) -> String {
     let mut result = String::new();
     loop {
@@ -19,8 +21,8 @@ fn column(mut index: u16) -> String {
 pub struct Position(u16, u16);
 
 impl Position {
-    pub fn new(mov: BigUint, size: u16) -> Position {
-        let index = mov.trailing_zeros().unwrap_or(0) as u16;
+    pub fn new(mov: Move, size: u16) -> Position {
+        let index = size.pow(2) - (mov.trailing_zeros().unwrap_or(0) as u16) - 1;
         Position(index / size, index % size)
     }
 }
