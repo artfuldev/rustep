28d0be3b7bcf88282f251c2e123b19feabe99a69
Sudarsan Balaji <hello@artful.dev>
2024-10-20 09:30:26 +0100

    Add a heuristic that caches the results of another

---
+++ src/evaluation/cached.rs
@@ -0,0 +1,27 @@
+use rustc_hash::FxHashMap;
+
+use crate::core::Game;
+
+use super::Heuristic;
+
+pub struct Cached(Box<dyn Heuristic>, FxHashMap<u64, i64>);
+impl Cached {
+    pub fn new(heuristic: Box<dyn Heuristic>) -> Self {
+        Self(heuristic, FxHashMap::default())
+    }
+}
+
+impl Heuristic for Cached {
+    fn score(&mut self, game: &Game) -> i64 {
+        match self.1.get(&game.hash) {
+            Some(score) => {
+                return *score;
+            }
+            None => {
+                let score = self.0.score(game);
+                self.1.insert(game.hash, score);
+                score
+            }
+        }
+    }
+}
--- src/evaluation/mod.rs
+++ src/evaluation/mod.rs
@@ -1,5 +1,7 @@
+pub mod cached;
 pub mod cumulative;
 pub mod heuristic;
 
+pub use cached::*;
 pub use cumulative::*;
 pub use heuristic::*;
