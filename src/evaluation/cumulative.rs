80b4098824bced5c75e63d2bb776ad9c14266e8f
Sudarsan Balaji <hello@artful.dev>
2024-10-20 09:34:44 +0100

    Stop exposing field

--- src/evaluation/cumulative.rs
+++ src/evaluation/cumulative.rs
@@ -2,7 +2,12 @@ use crate::core::Game;
 
 use super::Heuristic;
 
-pub struct Cumulative(pub Vec<Box<dyn Heuristic>>);
+pub struct Cumulative(Vec<Box<dyn Heuristic>>);
+impl Cumulative {
+    pub fn new(heuristics: Vec<Box<dyn Heuristic>>) -> Self {
+        Self(heuristics)
+    }
+}
 
 impl Heuristic for Cumulative {
     fn score(&mut self, game: &Game) -> i64 {
