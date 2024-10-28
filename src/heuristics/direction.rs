3ea95fd3e9208ee655bff428c575673cff261667
Sudarsan Balaji <hello@artful.dev>
2024-10-28 08:39:46 +0000

    Add direction

---
+++ src/heuristics/direction.rs
@@ -0,0 +1,18 @@
+pub enum Direction {
+    Horizontal,
+    Vertical,
+    Diagonal,
+    AntiDiagonal,
+}
+
+impl Direction {
+    #[inline(always)]
+    pub fn delta(&self) -> (i8, i8) {
+        match &self {
+            Self::Horizontal => (0, -1),
+            Self::Vertical => (-1, 0),
+            Self::Diagonal => (-1, -1),
+            Self::AntiDiagonal => (1, -1),
+        }
+    }
+}
--- src/heuristics/mod.rs
+++ src/heuristics/mod.rs
@@ -1,7 +1,9 @@
 pub mod cached;
 pub mod cumulative;
+pub mod direction;
 pub mod heuristic;
 
 pub use cached::*;
 pub use cumulative::*;
+pub use direction::*;
 pub use heuristic::*;
