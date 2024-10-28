0ac67bb92ec5cf15238e89874b96a7b0b22bde6b
Sudarsan Balaji <hello@artful.dev>
2024-10-28 09:05:41 +0000

    Borrow position and direction

--- src/heuristics/line.rs
+++ src/heuristics/line.rs
@@ -1,8 +1,10 @@
-use crate::{core::Position, heuristics::Direction};
+use crate::core::Position;
+
+use super::direction::Direction;
 
 pub type Line = Vec<Position>;
 
-pub fn line(start: Position, direction: Direction, win_length: u8, size: u8) -> Option<Line> {
+pub fn line(start: &Position, direction: &Direction, win_length: u8, size: u8) -> Option<Line> {
     let mut line = Vec::new();
     let mut row = start.0 as i16;
     let mut column = start.1 as i16;
@@ -31,84 +33,84 @@ mod tests {
     #[test]
     fn test_line_generates_horizontal_line() {
         let expected = Some(vec![Position(2, 2), Position(2, 1), Position(2, 0)]);
-        let actual = line(Position(2, 2), Direction::Horizontal, 3, 3);
+        let actual = line(&Position(2, 2), &Direction::Horizontal, 3, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_horizontal_line_of_win_length() {
         let expected = Some(vec![Position(2, 2), Position(2, 1)]);
-        let actual = line(Position(2, 2), Direction::Horizontal, 2, 3);
+        let actual = line(&Position(2, 2), &Direction::Horizontal, 2, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_no_horizontal_line_when_not_possible() {
         let expected = None;
-        let actual = line(Position(2, 2), Direction::Horizontal, 4, 3);
+        let actual = line(&Position(2, 2), &Direction::Horizontal, 4, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_vertical_line() {
         let expected = Some(vec![Position(2, 2), Position(1, 2), Position(0, 2)]);
-        let actual = line(Position(2, 2), Direction::Vertical, 3, 3);
+        let actual = line(&Position(2, 2), &Direction::Vertical, 3, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_vertical_line_of_win_length() {
         let expected = Some(vec![Position(2, 2), Position(1, 2)]);
-        let actual = line(Position(2, 2), Direction::Vertical, 2, 3);
+        let actual = line(&Position(2, 2), &Direction::Vertical, 2, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_no_vertical_line_when_not_possible() {
         let expected = None;
-        let actual = line(Position(2, 2), Direction::Vertical, 4, 3);
+        let actual = line(&Position(2, 2), &Direction::Vertical, 4, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_diagonal_line() {
         let expected = Some(vec![Position(2, 2), Position(1, 1), Position(0, 0)]);
-        let actual = line(Position(2, 2), Direction::Diagonal, 3, 3);
+        let actual = line(&Position(2, 2), &Direction::Diagonal, 3, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_diagonal_line_of_win_length() {
         let expected = Some(vec![Position(2, 2), Position(1, 1)]);
-        let actual = line(Position(2, 2), Direction::Diagonal, 2, 3);
+        let actual = line(&Position(2, 2), &Direction::Diagonal, 2, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_no_diagonal_line_when_not_possible() {
         let expected = None;
-        let actual = line(Position(2, 2), Direction::Diagonal, 4, 3);
+        let actual = line(&Position(2, 2), &Direction::Diagonal, 4, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_anti_line() {
         let expected = Some(vec![Position(2, 0), Position(1, 1), Position(0, 2)]);
-        let actual = line(Position(2, 0), Direction::AntiDiagonal, 3, 3);
+        let actual = line(&Position(2, 0), &Direction::AntiDiagonal, 3, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_anti_line_of_win_length() {
         let expected = Some(vec![Position(2, 0), Position(1, 1)]);
-        let actual = line(Position(2, 0), Direction::AntiDiagonal, 2, 3);
+        let actual = line(&Position(2, 0), &Direction::AntiDiagonal, 2, 3);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_line_generates_no_anti_line_when_not_possible() {
         let expected = None;
-        let actual = line(Position(2, 2), Direction::AntiDiagonal, 3, 3);
+        let actual = line(&Position(2, 2), &Direction::AntiDiagonal, 3, 3);
         assert_eq!(actual, expected);
     }
 }
