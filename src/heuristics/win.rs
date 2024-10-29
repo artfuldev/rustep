e3515645afa07de6b9a226cc5152797e8679afec
Sudarsan Balaji <hello@artful.dev>
2024-10-29 22:45:12 +0000

    Include the cost of moves

--- src/heuristics/win.rs
+++ src/heuristics/win.rs
@@ -7,14 +7,15 @@ impl Win {}
 
 impl Heuristic for Win {
     fn score(&mut self, game: &Game) -> i64 {
+        let cost = game.moves.len() as i64;
         match Termination::of(game) {
             None => 0,
             Some(Termination::Drawn) => 0,
             Some(Termination::Won(side)) => {
                 if side == Side::X {
-                    i64::MAX
+                    i64::MAX - cost
                 } else {
-                    i64::MIN
+                    i64::MIN + cost
                 }
             }
         }
