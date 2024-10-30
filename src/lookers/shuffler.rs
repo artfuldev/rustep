c4178a68aa0b396d63f9bd5bbec06a3d5d60b49b
Sudarsan Balaji <hello@artful.dev>
2024-10-30 00:48:56 +0000

    Allow constructing a shuffler

--- src/lookers/shuffler.rs
+++ src/lookers/shuffler.rs
@@ -7,6 +7,12 @@ use super::looker::Looker;
 
 pub struct Shuffler(Box<dyn Looker>, ThreadRng);
 
+impl Shuffler {
+    pub fn new(looker: Box<dyn Looker>, thread_rng: ThreadRng) -> Self {
+        Self(looker, thread_rng)
+    }
+}
+
 impl Looker for Shuffler {
     fn moves(&mut self, game: &Game) -> Vec<Position> {
         let slice: &mut [Position] = &mut self.0.moves(game);
