3f7b38f40b8463e689b306b890722e8a29d97b87
Sudarsan Balaji <hello@artful.dev>
2024-10-31 10:30:12 +0000

    Add a test

--- src/heuristics/win.rs
+++ src/heuristics/win.rs
@@ -26,3 +26,22 @@ impl Heuristic for Win {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::heuristics::Null;
+
+    use super::*;
+    use anyhow::Result;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_win_scores_premature_using_heuristic() -> Result<()> {
+        let (_, mut game) =
+            Game::parse("15_/14_o/15_/_o13_/15_/3_o3_x7_/15_/5_x_x7_/7_x7_/7_x7_/15_/8_x6_/15_/o6_2o6_/15_ x")?;
+        game.set_win_length(5);
+        let mut heuristic = Win::new(Box::new(Null));
+        assert_eq!(heuristic.score(&game), 0);
+        Ok(())
+    }
+}
